07bbbb336da610b33808232140f6ceed2ac1e082
Jack Darnell <darnelljack@icloud.com>
2023-02-24 19:26:21 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { parseArticle } from './lambdaFunctions.js';
+
+
+async function getArticle() {
+  const reply = await parseArticle('parser', { url: 'https://hackaday.com/2023/02/24/toroid-transformers-explained/' });
+  console.log(reply.Payload);
+}
 
 function App() {
+
+  //const data = await postData('https://anb34jlouk2jykl4364xlbddwu0exutn.lambda-url.us-east-2.on.aws/', { url: 'https://hackaday.com/2023/02/24/toroid-transformers-explained/' })
+
+  // postData('https://anb34jlouk2jykl4364xlbddwu0exutn.lambda-url.us-east-2.on.aws/', { url: 'https://hackaday.com/2023/02/24/toroid-transformers-explained/' })
+  // .then((data) => {
+  //   console.log(data); // JSON data parsed by `data.json()` call
+  // });
+  
+
+  getArticle();
+
   return (
     <div className="App">
       <header className="App-header">
