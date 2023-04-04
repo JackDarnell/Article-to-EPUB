6a2c12f42ad28892d27902d49c74ddb25726f93f
Jack Darnell <darnelljack@icloud.com>
2023-04-04 15:48:00 -0400

    removed unnecessary code

--- src/lambdaFunctions.js
+++ src/lambdaFunctions.js
@@ -1,16 +1,2 @@
 import AWS from 'aws-sdk';
 
-AWS.config.update({
-    region: 'us-east-2',
-    accessKeyId: 'AKIAQFNXA23WGX2XWLFA',
-    secretAccessKey: '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',
-  });
-
-  export const parseArticle = (functionName, payload) => {
-    const lambda = new AWS.Lambda();
-    const params = {
-      FunctionName: functionName,
-      Payload: JSON.stringify(payload),
-    };
-    return lambda.invoke(params).promise();
-  };
